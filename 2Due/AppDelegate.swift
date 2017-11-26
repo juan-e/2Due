d91fd66d2f8f6b1b0ac6fa22f65c8d0a5852ebba
joncv <joncv@umich.edu>
2017-11-26 12:09:42 -0500

    Initial Commit

---
+++ 2Due/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  2Due
+//
+//  Created by Jonathan Vredenburg on 11/26/17.
+//  Copyright © 2017 Jonathan Vredenburg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 2Due/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  2Due
+//
+//  Created by Jonathan Vredenburg on 11/26/17.
+//  Copyright © 2017 Jonathan Vredenburg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
